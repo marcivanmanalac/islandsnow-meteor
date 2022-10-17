44ce09c21f2c55f7ade27703921fc10eda63e8df
meezyack <marcivanmanalac@gmail.com>
2022-10-17 12:16:13 -1000

    Completed islandsnow mockup

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -3,39 +3,22 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
-import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
-import SignUp from '../pages/SignUp';
-import SignOut from '../pages/SignOut';
-import NavBar from '../components/NavBar';
-import SignIn from '../pages/SignIn';
-import NotAuthorized from '../pages/NotAuthorized';
-import IslandSnow from '../pages/IslandSnow';
 
+import IslandSnow from '../pages/IslandSnow';
+import FooterMenu from '../components/FooterMenu';
+import TopMenu from '../components/TopMenu';
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 const App = () => (
   <Router>
     <div className="d-flex flex-column min-vh-100">
-      <NavBar />
+      <TopMenu />
       <Routes>
         <Route exact path="/" element={<IslandSnow />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signout" element={<SignOut />} />
-        <Route path="/home" element={<ProtectedRoute><Landing /></ProtectedRoute>} />
-        <Route path="/list" element={<ProtectedRoute><ListStuff /></ProtectedRoute>} />
-        <Route path="/add" element={<ProtectedRoute><AddStuff /></ProtectedRoute>} />
-        <Route path="/edit/:_id" element={<ProtectedRoute><EditStuff /></ProtectedRoute>} />
-        <Route path="/admin" element={<AdminProtectedRoute><ListStuffAdmin /></AdminProtectedRoute>} />
-        <Route path="/notauthorized" element={<NotAuthorized />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
+      <FooterMenu />
     </div>
   </Router>
 );
--- app/imports/ui/pages/IslandSnow.jsx
+++ app/imports/ui/pages/IslandSnow.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import TopMenu from '../components/TopMenu';
 import Middle from '../components/Middle';
 import Logo from '../components/Logo';
 import MainImage from '../components/MainImage';
-import FooterMenu from '../components/FooterMenu';
 
 const IslandSnow = () => (
   <>
-    <TopMenu />
     <Logo />
     <Middle />
     <MainImage />
-    <FooterMenu />
   </>
 );
 
